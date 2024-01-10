e8897f9034c9d39b6291645ab8ba6a0d2af8081a
Marko Pavić <mapavic@vsite.hr>
2024-01-10 13:20:06 +0100

    Add project files.

---
+++ Vjezba6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vjezba6.Global" Language="C#" %>
---
+++ Vjezba6/Registracija.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Vjezba6
+{
+	public partial class Registracija : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+
+        }
+
+		protected void gvStudents_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void btnUpdate_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void btnSearch_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void btnDisplay_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void btnTotal_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void btnDelete_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
